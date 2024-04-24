db6920160c17728e8a9ff9c2e4850a33870ae5aa
Will S <williamsusanto82@gmail.com>
2024-04-24 20:54:33 +0700

    [DTO] Created DTO to get data from API

---
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/dto/GetProfileResponse.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.heymartstore.dto;
+
+public class GetProfileResponse
+{
+    public Long id;
+    public String email;
+    public String name;
+    public String role;
+}
