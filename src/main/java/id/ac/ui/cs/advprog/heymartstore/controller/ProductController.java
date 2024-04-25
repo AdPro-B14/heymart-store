2ec0d679ddf08f2cbd9b800b7b9bd8f3e42cd9b3
Will S <williamsusanto82@gmail.com>
2024-04-25 08:44:49 +0700

    [FIX] Fix query to find products based on the name

--- src/main/java/id/ac/ui/cs/advprog/heymartstore/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/controller/ProductController.java
@@ -5,6 +5,7 @@ import id.ac.ui.cs.advprog.heymartstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,7 +23,8 @@ public class ProductController {
     }
 
     @GetMapping("/search/{query}")
-    public ResponseEntity<List<Product>> queryProduct(String query) {
+    public ResponseEntity<List<Product>> queryProduct(@PathVariable("query") String query) {
+        System.out.println(query);
         List<Product> queryProduct = productService.searchProduct(query);
         return ResponseEntity.ok(queryProduct);
     }
