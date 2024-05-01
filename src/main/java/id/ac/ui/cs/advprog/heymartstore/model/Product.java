be027a4a5dccc8e51f90706423c87c50efe48a39
ArvinCS <arvinciu86@yahoo.com>
2024-05-01 18:15:10 +0700

    [FIX] Cycle dependency supermarket-product

--- src/main/java/id/ac/ui/cs/advprog/heymartstore/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/model/Product.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.heymartstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,8 +22,9 @@ public class Product {
     @Column(name = "stock", nullable = false)
     private Integer stock;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "supermarket_id", nullable = false)
+    @JsonIgnoreProperties(value = {"products", "handler", "hibernateLazyInitializer"}, allowSetters = true)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private Supermarket supermarket;
