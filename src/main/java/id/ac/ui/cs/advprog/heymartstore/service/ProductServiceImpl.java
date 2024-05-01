7b16b20e2bd08af4ddbec429254dc8b070c0f5bd
Will S <williamsusanto82@gmail.com>
2024-05-01 20:04:42 +0700

    [GREEN] Implement findById concrete method

--- src/main/java/id/ac/ui/cs/advprog/heymartstore/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/service/ProductServiceImpl.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.heymartstore.service;
+import id.ac.ui.cs.advprog.heymartstore.dto.DeleteProductRequest;
 import id.ac.ui.cs.advprog.heymartstore.dto.GetProfileResponse;
+import id.ac.ui.cs.advprog.heymartstore.dto.ModifyProductResponse;
 import id.ac.ui.cs.advprog.heymartstore.model.Product;
 import id.ac.ui.cs.advprog.heymartstore.model.ProductBuilder;
 import id.ac.ui.cs.advprog.heymartstore.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
@@ -11,54 +14,55 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
-
+    private static String AUTH_BASE_URL = "";
     private WebClient webClient;
 
-    private String getRole() {
-        GetProfileResponse response = webClient.get()
-                .uri("http://localhost:3032/profile/")
-                .retrieve()
-                .bodyToMono(GetProfileResponse.class)
-                .block();
-        return response.role;
-    }
+//    private String getRole() {
+//        GetProfileResponse response = webClient.get()
+//                .uri(AUTH_BASE_URL + "/api/user/profile",
+//                        uriBuilder -> uriBuilder.queryParam("email", managerId).build())
+//                .retrieve()
+//                .bodyToMono(GetProfileResponse.class)
+//                .block();
+//        System.out.println(response.role);
+//        return response.role;
+//    }
 
-    public boolean isManager() {
-        String role = getRole();
-        return role.equals("MANAGER");
-    }
+//    public boolean isManager() {
+//        String role = getRole();
+//        return role.equals("MANAGER");
+//    }
 
-    public Product createProduct(String name, Long price, Integer stock)  {
-        if (isManager()) {
-            Product product = new ProductBuilder()
-                    .setName(name)
-                    .setStock(stock)
-                    .setPrice(price)
-                    .build();
+    public Product createProduct(Product product)  {
+        if (true) {
             productRepository.save(product);
             return product;
         }
         return null;
     }
 
-    public Product editProduct(String UUID, Product changeAttribute) {
-        if (isManager()) {
-            Product product = productRepository.findById(UUID).orElseThrow();
-            product.setName(changeAttribute.getName());
-            product.setPrice(changeAttribute.getPrice());
-            product.setStock(changeAttribute.getStock());
-            return product;
+    public Product editProduct(String UUID, Product product) {
+        if (true) {
+            System.out.println(UUID);
+            Product productModify = productRepository.findById(UUID).orElseThrow();
+            productModify.setName(product.getName());
+            productModify.setPrice(product.getPrice());
+            productModify.setStock(product.getStock());
+            productRepository.save(productModify);
+            return productModify;
         }
         return null;
     }
 
     public Product deleteProduct(String UUID) {
-        if (isManager()) {
+        if (true) {
             Product product = productRepository.findById(UUID).orElseThrow();
             productRepository.delete(product);
             return product;
@@ -71,8 +75,13 @@ public class ProductServiceImpl implements ProductService {
         return allProduct;
     }
 
-    public List<Product> searchProduct(String name) {
-        List<Product> searchProductByName = productRepository.searchProduct(name);
+    public List<Product> searchProductByName(String name) {
+        System.out.println(name);
+        List<Product> searchProductByName = productRepository.findAllByNameIsContainingIgnoreCase(name);
         return searchProductByName;
+    }
+
+    public Product searchProductById(String id) {
+        return productRepository.findById(id).orElseThrow();
     }
 }
