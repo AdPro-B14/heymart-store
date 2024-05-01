3276b9d5f151d5756f75a22d8c38d2586965e477
Will S <williamsusanto82@gmail.com>
2024-05-01 20:02:09 +0700

    [RED] Create test case for findById

--- src/test/java/id/ac/ui/cs/advprog/heymartstore/service/ProductServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/heymartstore/service/ProductServiceTest.java
@@ -54,6 +54,18 @@ public class ProductServiceTest {
         assertEquals(productList.getFirst().getName(),"Indomie");
         assertEquals(productList.getFirst().getPrice(), 10000L);
         assertEquals(productList.getFirst().getStock(), 10);
+    }
 
+    @Test
+    public void testFindProductById() {
+        Product product = new ProductBuilder()
+                .setName("Indomie")
+                .setPrice(10000L)
+                .setStock(10)
+                .build();
+        product.setId("123");
+        productList.add(product);
+
+        assertEquals("123", productList.getFirst().getId());
     }
 }
