2482328a41a0e9f54d824c53f0f1034b3e85ee3d
Will S <williamsusanto82@gmail.com>
2024-05-01 20:04:16 +0700

    [GREEN] Implement ProductService interface for findById

--- src/main/java/id/ac/ui/cs/advprog/heymartstore/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/service/ProductService.java
@@ -1,13 +1,15 @@
 package id.ac.ui.cs.advprog.heymartstore.service;
 
+import id.ac.ui.cs.advprog.heymartstore.dto.ModifyProductResponse;
 import id.ac.ui.cs.advprog.heymartstore.model.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    public Product createProduct(String name, Long price, Integer stock);
-    public Product editProduct(String UUID, Product changeAttribute);
+    public Product createProduct(Product product);
+    public Product editProduct(String UUID, Product product);
     public Product deleteProduct(String UUID);
     public List<Product> getAllProduct();
-    public List<Product> searchProduct(String name);
+    public List<Product> searchProductByName(String name);
+    public Product searchProductById(String id);
 }
