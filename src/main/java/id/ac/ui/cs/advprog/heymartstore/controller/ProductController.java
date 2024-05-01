34c044f1df54116a2cf1491706c62ed0a1d8d589
Will S <williamsusanto82@gmail.com>
2024-05-01 20:00:50 +0700

    [CONTROLLER] Added new controller for findById

--- src/main/java/id/ac/ui/cs/advprog/heymartstore/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/controller/ProductController.java
@@ -1,15 +1,21 @@
 package id.ac.ui.cs.advprog.heymartstore.controller;
 
+import id.ac.ui.cs.advprog.heymartstore.dto.CreateProductRequest;
+import id.ac.ui.cs.advprog.heymartstore.dto.DeleteProductRequest;
+import id.ac.ui.cs.advprog.heymartstore.dto.ModifyProductResponse;
 import id.ac.ui.cs.advprog.heymartstore.model.Product;
+import id.ac.ui.cs.advprog.heymartstore.model.ProductBuilder;
 import id.ac.ui.cs.advprog.heymartstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@RequestMapping("/product")
 @RestController
 public class ProductController {
 
@@ -24,8 +30,40 @@ public class ProductController {
 
     @GetMapping("/search/{query}")
     public ResponseEntity<List<Product>> queryProduct(@PathVariable("query") String query) {
-        System.out.println(query);
-        List<Product> queryProduct = productService.searchProduct(query);
+        List<Product> queryProduct = productService.searchProductByName(query);
+        return ResponseEntity.ok(queryProduct);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequest product) {
+        Product savedProduct = productService.createProduct(new ProductBuilder()
+                .setName(product.name)
+                .setStock(product.stock)
+                .setPrice(product.price)
+                .build());
+        return new ResponseEntity<>(savedProduct, HttpStatus.OK);
+    }
+
+    @PostMapping("/edit")
+    public ResponseEntity<Product> editProduct(@RequestBody ModifyProductResponse product) {
+        Product savedProduct = productService.editProduct(product.UUID,
+                new ProductBuilder()
+                        .setName(product.name)
+                        .setPrice(product.price)
+                        .setStock(product.stock)
+                        .build());
+        return new ResponseEntity<>(savedProduct, HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<Product> deleteProduct(@RequestBody DeleteProductRequest request) {
+        Product savedProduct = productService.deleteProduct(request.UUID);
+        return new ResponseEntity<>(savedProduct, HttpStatus.OK);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Product> searchProductById(@PathVariable("id") String id) {
+        Product queryProduct = productService.searchProductById(id);
         return ResponseEntity.ok(queryProduct);
     }
 }
