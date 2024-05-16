769a296858459546c5b42d7365a2c085c9b2f499
Will S <williamsusanto82@gmail.com>
2024-05-16 13:27:08 +0700

    [REFACTOR] Create new Jpa method to find product by name and in a spesific supermarket

--- src/main/java/id/ac/ui/cs/advprog/heymartstore/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.heymartstore.repository;
 import id.ac.ui.cs.advprog.heymartstore.model.Product;
+import id.ac.ui.cs.advprog.heymartstore.model.Supermarket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,5 +8,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
-    public List<Product> findAllByNameIsContainingIgnoreCase(String name);
+    List<Product> findBySupermarketAndNameContainingIgnoreCase(Supermarket supermarket, String name);
 }
