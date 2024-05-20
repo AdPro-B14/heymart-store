21d5cc0f2caa2331924c5dee0e048a4a59b83ce0
Will S <williamsusanto82@gmail.com>
2024-05-21 05:11:47 +0700

    [TEST] Create high coverage for service testing

--- src/test/java/id/ac/ui/cs/advprog/heymartstore/service/ProductServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/heymartstore/service/ProductServiceTest.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.heymartstore.model.Product;
 import id.ac.ui.cs.advprog.heymartstore.model.ProductBuilder;
 import id.ac.ui.cs.advprog.heymartstore.model.Supermarket;
 import id.ac.ui.cs.advprog.heymartstore.repository.ProductRepository;
+import id.ac.ui.cs.advprog.heymartstore.repository.SupermarketRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,21 +18,26 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
     @Mock
     ProductRepository productRepository;
+
+    @Mock
+    SupermarketRepository supermarketRepository;
+
     @InjectMocks
     ProductServiceImpl service;
 
-    private static ArrayList<Product> productList;
+    private static List<Product> productList;
     private static ArrayList<Supermarket> supermarketList;
 
     @BeforeEach
@@ -46,6 +52,12 @@ public class ProductServiceTest {
         productList = new ArrayList<>();
     }
 
+
+    private Product createProduct(Product product) {
+        productList.add(product);
+        return product;
+    }
+
     @Test
     public void testCreateProduct() {
         Product product = new ProductBuilder()
@@ -53,10 +65,78 @@ public class ProductServiceTest {
                 .setPrice(10000L)
                 .setStock(10)
                 .build();
-        productList.add(product);
+        when(productRepository.save(product)).thenReturn(createProduct(product));
+        service.createProduct(product);
         assertEquals(productList.getFirst().getName(),"Indomie");
         assertEquals(productList.getFirst().getPrice(), 10000L);
         assertEquals(productList.getFirst().getStock(), 10);
+    }
+
+    private Product editProduct(Product product, String name) {
+        product.setName(name);
+        productList.add(product);
+        return product;
+    }
+
+    @Test
+    public void testEditProduct() {
+        Product product = new ProductBuilder()
+                .setName("Indomie")
+                .setPrice(10000L)
+                .setStock(10)
+                .build();
+        product.setId("123");
+
+        assertEquals(product.getName(),"Indomie");
+
+        when(productRepository.save(product)).thenReturn(editProduct(product, "Ayam"));
+        when(productRepository.findById("123")).thenReturn(Optional.of(product));
+        service.editProduct("123", product);
+
+        assertEquals(productList.getFirst().getName(),"Ayam");
+        assertEquals(productList.getFirst().getPrice(), 10000L);
+        assertEquals(productList.getFirst().getStock(), 10);
+    }
+
+    private boolean deleteProduct(Product product) {
+        productList.remove(product);
+        return true;
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Product product = new ProductBuilder()
+                .setName("Indomie")
+                .setPrice(10000L)
+                .setStock(10)
+                .build();
+        product.setId("123");
+
+        when(productRepository.findById("123")).thenReturn(Optional.of(product));
+        service.deleteProduct("123");
+
+        verify(productRepository, times(1)).delete(product);
+
+        deleteProduct(product);
+
+        assertEquals(0, productList.size());
+    }
+
+    @Test
+    public void testGetAllProduct() {
+        Product product1 = new ProductBuilder()
+                .setName("Indomie")
+                .setPrice(10000L)
+                .setStock(10)
+                .build();
+        productList.add(product1);
+        Supermarket supermarket = new Supermarket();
+        supermarket.setProducts(productList);
+        when(supermarketRepository.getReferenceById(1L)).thenReturn(supermarket);
+        List<Product> product = service.getAllProduct(1L);
+        assertEquals("Indomie", product.getFirst().getName());
+        assertEquals(10000L, product.getFirst().getPrice());
+        assertEquals(10, product.getFirst().getStock());
     }
 
     @Test
@@ -69,7 +149,10 @@ public class ProductServiceTest {
         product.setId("123");
         productList.add(product);
 
-        assertEquals("123", productList.getFirst().getId());
+        when(productRepository.findById("123")).thenReturn(Optional.of(product));
+
+        Product queryProduct = service.searchProductById("123");
+        assertEquals("123", queryProduct.getId());
     }
 
     @Test
@@ -92,8 +175,11 @@ public class ProductServiceTest {
         productList.add(product);
         supermarketList.add(supermarket1);
 
+        when(productRepository.findBySupermarketAndNameContainingIgnoreCase(supermarketList.getFirst(), "Ind")).
+                thenReturn(supermarketList.getFirst().getProducts());
+
         assertEquals(supermarketList.getFirst(), product.getSupermarket());
-        List<Product> queryProduct = service.searchProductByName(supermarket1, product.getName());
-        assertTrue(productList.getFirst().getName().contains(product.getName()));
+        List<Product> queryProduct = service.searchProductByName(supermarket1, "Ind");
+        assertTrue(queryProduct.getFirst().getName().contains(product.getName()));
     }
 }
