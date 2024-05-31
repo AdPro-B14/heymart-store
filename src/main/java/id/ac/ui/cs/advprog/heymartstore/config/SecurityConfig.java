1ddc57b9e7b06f7d632a3cbe183621c1e59ed90f
ArvinCS <arvinciu86@yahoo.com>
2024-05-31 14:47:39 +0700

    fix: cors error

---
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.heymartstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://hey-mart.vercel.app")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
+    }
+}
